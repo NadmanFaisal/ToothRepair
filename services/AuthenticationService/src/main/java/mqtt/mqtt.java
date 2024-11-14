f06074d8168522fe224111be8f80b8131781dc68
Danis Music <danism@student.chalmers.se>
2024-11-14 23:09:01 +0100

    #7 Correct filepath to java code to ensure Maven functionality

--- services/AuthenticationService/src/main/java/mqtt/mqtt.java
+++ services/AuthenticationService/src/main/java/mqtt/mqtt.java
@@ -1,4 +1,4 @@
-package services.AuthenticationService.mqtt;
+package main.java.mqtt;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
