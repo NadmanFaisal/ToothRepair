7c3f6959a0ec3769db765c8b077de347984701b2
Danis Music <danism@student.chalmers.se>
2024-11-18 18:55:12 +0100

    #8 Remove messagehandler & reformat method structure
    Co-authored-by: @puram @filipero

--- server/mqtt/mqtt.js
+++ server/mqtt/mqtt.js
@@ -2,26 +2,39 @@
 // Import mqtt
 const mqtt = require('mqtt');
 
-// Connect to the Mosquitto broker (localhost in this case)
+const client = mqtt.connect('mqtt://test.mosquitto.org:1883');
 
-const client = mqtt.connect('mqtt://localhost:1883');
-
+//Note: Separate the mqtt connection and connecting to a specific topic
 // Handle connection
 function connectToTopic(topic){
+    client.on('connect', () => {
+      console.log('Connected to Mosquitto broker');
+  
+      // Subscribe to a topic
+      client.subscribe(topic , (err) => {
+        if (err) {
+          console.error('Failed to subscribe:', err);
+        } else {
+          console.log(`Subscribed to ${topic}`);
+        }
+      });
+  
+    });
 
-client.on('connect', () => {
-  console.log('Connected to Mosquitto broker');
+    client.on('message', (topic, message) => {
+      console.log(`Received message: ${message.toString()} on topic: ${topic}`);
+      try{
+        message = JSON.parse(message.toString());
+        console.log("This is the JSON format of the patient list" + message)
+        console.log(message);
+      }catch(error){
+        console.error('Error Parsing the Patient list', error)
+      }
+    });
 
-  // Subscribe to a topic
-  client.subscribe(topic , (err) => {
-    if (err) {
-      console.error('Failed to subscribe:', err);
-    } else {
-      console.log('Subscribed to test/topic');
-    }
-  });
-
-})
+    client.on('error', (error) => {
+      console.error('MQTT Error:', error);
+    });
 }
 
 function publishToTopic(topic){ 
@@ -30,28 +43,10 @@ function publishToTopic(topic){
   });
 }
 
-// Handle incoming messages
-function handleIncomingMessage(topic){
-  client.on('message', (topic, message) => {
-    console.log(`Received message: ${message.toString()} on topic: ${topic}`);
-    try{
-      message = JSON.parse(message.toString());
-      console.log("This is the JSON format of the patient list" + message)
-      return message;
-    }catch(error){
-      console.error('Error Parsing the Patient list', error)
-    }
-  });
-}
-
 // Handle errors
-client.on('error', (error) => {
-  console.error('MQTT Error:', error);
-});
 
 module.exports = {
   client,
   connectToTopic,
   publishToTopic,
-  handleIncomingMessage
 }
