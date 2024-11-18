9d098d6109590bf919eb54956be21f9bafb0609c
Danis Music <danism@student.chalmers.se>
2024-11-18 18:50:24 +0100

    #8 Export route to index.js file
    Co-authored-by: @puram @filipero

--- server/routes/PatientRoutes.js
+++ server/routes/PatientRoutes.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const patientController = require('../controllers/PatientController')
+const mqtt = require('mqtt')
 
-router.get('/patients/', patientController.getPatients);
+router.get('/patients', patientController.getPatients);
+
+
+module.exports = router;
