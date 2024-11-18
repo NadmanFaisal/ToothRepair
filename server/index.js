eaf1e4fcdd476f76e5faeacd9c1cbc988553866a
Danis Music <danism@student.chalmers.se>
2024-11-18 18:53:40 +0100

    #8 Use route when running server
    Co-authored-by: @puram @filipero

--- server/index.js
+++ server/index.js
@@ -4,35 +4,32 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (origin === 'http://localhost:5001' || origin === 'http://192.168.10.238:8080') {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }, 
-  credentials: true,               
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], 
-  allowedHeaders: ['Content-Type', 'Authorization'],             
-};
 const app = express();
 const PORT = 5001;
 
-// Middleware
-app.use(express.json());
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true
+}
 
+var patientRoute = require('./routes/PatientRoutes')
+// Middleware
 app.use(cors(corsOptions));
+
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
 
 // Simple Route
 app.get('/', (req, res) => {
   res.send('Hello from the MEVN backend!');
 });
+
+
+app.use(patientRoute)
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
