36d957ec89a7a61a34e1eae9169a9f930fbc5d67
Danis Music <danism@student.chalmers.se>
2024-11-19 10:13:04 +0100

    #8 Reformat mqtt file to make sure that the connection happens only once
    Co-authored-by: @nadman

--- server/mqtt/mqtt.js
+++ server/mqtt/mqtt.js
@@ -6,39 +6,43 @@ const client = mqtt.connect('mqtt://test.mosquitto.org:1883');
 
 //Note: Separate the mqtt connection and connecting to a specific topic
 // Handle connection
-function connectToTopic(topic){
-    client.on('connect', () => {
-      console.log('Connected to Mosquitto broker');
-  
-      // Subscribe to a topic
-      client.subscribe(topic , (err) => {
-        if (err) {
-          console.error('Failed to subscribe:', err);
-        } else {
-          console.log(`Subscribed to ${topic}`);
-        }
-      });
-  
-    });
+client.on('connect', () => {
+  console.log('Connected to Mosquitto broker');
+});
 
+client.on('error', (error) => {
+  console.error('MQTT Error:', error);
+});
+
+function connectToTopic(topic){
+  return new Promise((resolve, reject) => {
+    // Subscribe to a topic
+    client.subscribe(topic , (err) => {
+      if (err) {
+        console.error('Failed to subscribe:', err);
+        return reject(err)
+      } else {
+        console.log(`Subscribed to ${topic}`);
+      }
+    });
+    
     client.on('message', (topic, message) => {
       console.log(`Received message: ${message.toString()} on topic: ${topic}`);
       try{
         message = JSON.parse(message.toString());
         console.log("This is the JSON format of the patient list" + message)
-        console.log(message);
+        resolve(message)
       }catch(error){
         console.error('Error Parsing the Patient list', error)
+        reject(error)
       }
     });
-
-    client.on('error', (error) => {
-      console.error('MQTT Error:', error);
-    });
+  
+  })
 }
 
 function publishToTopic(topic){ 
-    client.on('connect', () => {
+  client.on('connect', () => {
     client.publish(topic , 'Hello from mqtt.js!');
   });
 }
