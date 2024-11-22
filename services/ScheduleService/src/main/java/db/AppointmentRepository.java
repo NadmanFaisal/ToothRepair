a1f059e36da774b38c509b136b819c7da6b08aa0
Nadman Faisal <nadman@chalmers.se>
2024-11-22 11:44:36 +0100

    #9 Create AppointmentRepository for DB interaction

--- services/ScheduleService/src/main/java/db/AppointmentRepository.java
+++ services/ScheduleService/src/main/java/db/AppointmentRepository.java
@@ -1,5 +1,6 @@
 package main.java.db;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public class AppointmentRepository {
-    
+public interface AppointmentRepository extends MongoRepository<AppointmentSchema, String> {
+
 }
