14487658639bf0873e0b2b57a8e287eb6d4e907b
Nadman Faisal <nadman@chalmers.se>
2024-11-22 12:20:06 +0100

    #9 Implement booking service

--- services/ScheduleService/src/main/java/service/BookingService.java
+++ services/ScheduleService/src/main/java/service/BookingService.java
@@ -1,8 +1,27 @@
 package main.java.service;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import main.java.db.AppointmentRepository;
+import main.java.db.AppointmentSchema;
 
+@Service
 public class BookingService {
+
+    private AppointmentRepository appointmentRepository;
+    @Autowired
+    public BookingService(AppointmentRepository appointmentRepository) {
+        this.appointmentRepository = appointmentRepository;
+    }
+
+    public List<AppointmentSchema> getAllAppointments() {
+        return appointmentRepository.findAll();
+    }
+
+    public AppointmentSchema createAppointment(AppointmentSchema appointment) {
+        return appointmentRepository.save(appointment);
+    }
     
 }
