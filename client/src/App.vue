8deea5fec3947a5686c25104726e1803bb7edb59
Vaibhav Puram <guspurva@student.gu.se>
2024-11-24 15:27:36 +0100

    #6 Setup basic file structure in frontend for authentication
    Co-authored by: @mohamedt

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,13 @@
 <template>
-  
-  <router-link to="/signUpPage">Log In Page</router-link>
-  <div></div>
-  <router-link to="/logInPage">Sign Up Page</router-link>
-  <div>
-    <router-view/>
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
+  name: 'App',
   components: {
     HelloWorld
   }
