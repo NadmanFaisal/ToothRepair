ec345371249cb7b0eed07c783947d60d24607d63
Danis Music <danism@student.chalmers.se>
2024-11-24 10:01:13 +0100

    #10 Add preliminary map component

---
+++ client/src/components/Map.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="mapContainer" ref="map"></div>
+</template>
+
+<script>
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+
+export default {
+  name: 'MapComponent',
+  data() {
+    return {
+      map: null
+    }
+  },
+  mounted() {
+    this.map = L.map(this.$refs.map, L.CRS.Simple).setView([57.708870, 11.974560], 7)
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      noWrap: true
+    }).addTo(this.map)
+  }
+}
+
+</script>
+<style>
+    .mapContainer {
+        width: 800px;
+        height: 500px;
+        position: relative;
+        overflow: hidden;
+        border-style: solid;
+    }
+</style>
