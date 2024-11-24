0124fa28296f30ff338c652ec23d7140c133a395
Danis Music <danism@student.chalmers.se>
2024-11-24 10:00:47 +0100

    #10 Modify main file to follow proper spring boot running procedure

--- services/ClinicService/src/main/java/Main.java
+++ services/ClinicService/src/main/java/Main.java
@@ -1,22 +1,20 @@
-package main.java.mqtt;
-
+package main.java;
 import org.eclipse.paho.client.mqttv3.MqttException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+import main.java.mqtt.MQTT;
+import main.java.service.ClinicService;
+
+
+@SpringBootApplication
 public class Main {
+    private static ClinicService clinicService;
+    
     public static void main(String[] args) {
-        try {
-        
-            // Keep the client running to listen for incoming messages
-            new MQTTSubscriber();
-            // Keep the client running to listen for incoming messages
-
-            new MQTTPublisher();
-            
-            Thread.sleep(5000);
-        
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    
+        SpringApplication.run(Main.class, args);
 
     }
     
