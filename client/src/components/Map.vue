33ba8586103effd5868358a6f7027bac4ca993fc
Danis Music <danism@student.chalmers.se>
2024-11-25 21:48:01 +0100

    #10 Add methods to handle placing markers on clinic location

--- client/src/components/Map.vue
+++ client/src/components/Map.vue
@@ -8,9 +8,39 @@ import L from 'leaflet'
 
 export default {
   name: 'MapComponent',
+  props: {
+    clinics: {
+      type: Array,
+      required: true
+    }
+  },
   data() {
     return {
-      map: null
+      map: null,
+      markerGroup: null
+    }
+  },
+  methods: {
+    addMarkers() {
+      if (this.markerGroup) {
+        this.markerGroup.clearLayers()
+      } else {
+        this.markerGroup = L.layerGroup().addTo(this.map)
+      }
+      console.log('Inside addmarker, check clinics: ' + this.clinics)
+      this.clinics.forEach(clinic => {
+        const marker = L.marker([clinic.coordinate.latitude, clinic.coordinate.longitude]).addTo(this.map)
+        marker.bindPopup(clinic.name)
+        this.markerGroup.addLayer(marker)
+      })
+    }
+  },
+  watch: {
+    clinics: {
+      handler(newClinics) {
+        console.log('Clinics updated, adding markers: ', newClinics)
+        this.addMarkers()
+      }
     }
   },
   mounted() {
