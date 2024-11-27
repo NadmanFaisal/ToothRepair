84cd8925cb44aa350b8af79e1cf5f06be73d2897
Nadman Faisal <nadman@chalmers.se>
2024-11-27 18:30:01 +0100

    #9 Create the top bar

--- client/src/views/PatientHomePage.vue
+++ client/src/views/PatientHomePage.vue
@@ -1,5 +1,26 @@
 <template>
-    <div>This is the patient homepage</div>
+    <div class="row">
+        <div class="col-12 top-bar-container">
+
+            <div class="col-3 logo-container">
+                <img src="../assets/app-logo.png" class="logo">
+                <label class="logo-label">TEETH REPAIR</label>
+            </div>
+
+            <div class="col-7 router-container">
+            </div>
+
+            <div class="col-2 settings-container">
+                <img src="../assets/notifications.png" class="notification-label">
+                <img src="../assets/profile-picture.png" class="profile-picture">
+                <label class="patient-name-label"> Name of Patient</label>
+            </div>
+
+        </div>
+
+        <div class="col-12 content-section">
+        </div>
+    </div>
 </template>
 
 <script>
@@ -7,3 +28,71 @@ export default {
   name: 'PatientHomePage'
 }
 </script>
+
+<style scoped>
+.top-bar-container {
+    display: flex;
+    flex-direction: row;
+    background-color: white;
+    height: 70px;
+}
+
+.logo-container {
+    margin-left: 15px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+.logo {
+    height: 45px;
+    width: 30px;
+}
+
+.logo-label {
+    margin-left: 5px;
+    color: #1FC2C2;
+    text-align: center;
+    font-family: Inter;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 900;
+    line-height: normal;
+}
+
+.settings-container {
+    margin-left: 15px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+.notification-label {
+    height: 35px;
+    width: 35px;
+}
+
+.profile-picture {
+    margin-left: 10px;;
+    height: 50px;
+    width: 50px;
+}
+
+.patient-name-label {
+    margin-left: 10px;
+    color: #000;
+    text-align: center;
+    font-family: Inter;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+}
+
+.content-section {
+    display: flex;
+    flex-direction: row;
+    height: 100vh;
+    background-color: black;
+}
+</style>
