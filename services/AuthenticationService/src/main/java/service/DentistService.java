128185d52c7143402fdfe904ba9415d4d9c71047
Mohamed Taha Jasser <mohamedt@student.chalmers.se>
2024-11-27 15:45:17 +0100

    #6 Add communication between DentistSchema and DentistDatabase using DentistRepository and DentistService
    Co-authored by: @puram

---
+++ services/AuthenticationService/src/main/java/service/DentistService.java
@@ -0,0 +1,29 @@
+package main.java.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import main.java.db.DentistRepository;
+import main.java.db.DentistSchema;
+
+@Service
+public class DentistService {
+
+    private DentistRepository dentistRepository;
+    @Autowired
+    public DentistService(DentistRepository dentistRepository) {
+        this.dentistRepository = dentistRepository;
+    }
+
+    public List<DentistSchema> getAllDentists() {
+        return dentistRepository.findAll();
+        
+    }
+
+    public DentistSchema createDentist(DentistSchema dentist) {
+        return dentistRepository.save(dentist);
+    }
+
+}
