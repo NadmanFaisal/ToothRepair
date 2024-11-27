7c7642b57992cdff0d5db43db1c1d5c157a85aad
Nadman Faisal <nadman@chalmers.se>
2024-11-27 20:09:33 +0100

    #9 Create container for different components

---
+++ client/src/components/TopBar.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="col-12 top-bar-container">
+
+        <div class="col-3 logo-container">
+            <img src="../assets/app-logo.png" class="logo">
+            <label class="logo-label">TEETH REPAIR</label>
+        </div>
+
+        <div class="col-6 router-container">
+        </div>
+
+        <div class="col-3 settings-container">
+            <img src="../assets/notifications.png" class="notification-label">
+            <img src="../assets/profile-picture.png" class="profile-picture">
+            <label class="patient-name-label"> Name of Patient</label>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'TopBarComponent'
+}
+</script>
+
+<style scoped>
+.top-bar-container {
+    display: flex;
+    flex-direction: row;
+    background-color: white;
+    height:7%;
+}
+
+.logo-container {
+    margin-left: 15px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+.logo {
+    height: 45px;
+    width: 30px;
+}
+
+.logo-label {
+    margin-left: 5px;
+    color: #1FC2C2;
+    text-align: center;
+    font-family: Inter;
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 900;
+    line-height: normal;
+}
+
+.settings-container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+
+.notification-label {
+    margin-left: 150px;
+    height: 35px;
+    width: 35px;
+}
+
+.profile-picture {
+    margin-left: 10px;;
+    height: 50px;
+    width: 50px;
+}
+
+.patient-name-label {
+    margin-left: 10px;
+    color: #000;
+    text-align: center;
+    font-family: Inter;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+}
+</style>
--- client/src/views/PatientHomePage.vue
+++ client/src/views/PatientHomePage.vue
@@ -1,31 +1,43 @@
 <template>
     <div class="row">
-        <div class="col-12 top-bar-container">
 
-            <div class="col-3 logo-container">
-                <img src="../assets/app-logo.png" class="logo">
-                <label class="logo-label">TEETH REPAIR</label>
-            </div>
-
-            <div class="col-6 router-container">
-            </div>
-
-            <div class="col-3 settings-container">
-                <img src="../assets/notifications.png" class="notification-label">
-                <img src="../assets/profile-picture.png" class="profile-picture">
-                <label class="patient-name-label"> Name of Patient</label>
-            </div>
-
-        </div>
+        <TopBarComponent />
 
         <div class="col-12 content-section">
+
+            <div class="col-6 left-section">
+
+                <div class="col-6 calender-contaiener">
+                    container 1
+                </div>
+
+                <div class="col-6 map-container">
+                    container 2
+                </div>
+
+            </div>
+
+            <div class="col-6 right-section">
+
+                <div class="col-6 appointment-container">
+                    container 3
+                </div>
+
+            </div>
+
         </div>
     </div>
 </template>
 
 <script>
+
+import TopBarComponent from '../components/TopBar.vue'
+
 export default {
-  name: 'PatientHomePage'
+  name: 'PatientHomePage',
+  components: {
+    TopBarComponent
+  }
 }
 </script>
 
@@ -37,70 +49,32 @@ export default {
     height: 100vh;
 }
 
-.top-bar-container {
-    display: flex;
-    flex-direction: row;
-    background-color: white;
-    height:7%;
-}
-
-.logo-container {
-    margin-left: 15px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-}
-
-.logo {
-    height: 45px;
-    width: 30px;
-}
-
-.logo-label {
-    margin-left: 5px;
-    color: #1FC2C2;
-    text-align: center;
-    font-family: Inter;
-    font-size: 24px;
-    font-style: normal;
-    font-weight: 900;
-    line-height: normal;
-}
-
-.settings-container {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-}
-
-.notification-label {
-    margin-left: 150px;
-    height: 35px;
-    width: 35px;
-}
-
-.profile-picture {
-    margin-left: 10px;;
-    height: 50px;
-    width: 50px;
-}
-
-.patient-name-label {
-    margin-left: 10px;
-    color: #000;
-    text-align: center;
-    font-family: Inter;
-    font-size: 16px;
-    font-style: normal;
-    font-weight: 700;
-    line-height: normal;
-}
-
 .content-section {
     display: flex;
     flex-direction: row;
     height: 93%;
     background-image: linear-gradient(rgba(62, 180, 137, 0.5), rgba(62, 180, 137, 0.5)), url('@/assets/patient-home-bg-image.jpeg');
     background-size: cover;
+}
+
+.calender-contaiener {
+    display: flex;
+    flex-direction: column;
+    height: 50%;
+    width: 100%;
+}
+
+.map-container {
+    display: flex;
+    flex-direction: column;
+    height: 50%;
+    width: 100%
+}
+
+.appointment-container {
+    display: flex;
+    flex-direction: column;
+    height: 50%;
+    width: 100%
 }
 </style>
