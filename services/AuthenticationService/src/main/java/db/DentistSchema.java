ca53ad05fed87dc9995e71d8efde7102e55b3cc7
Mohamed Taha Jasser <mohamedt@student.chalmers.se>
2024-11-27 15:42:56 +0100

    #6 Add dentistSchema to the Authentication Service
    Co-authored by: @puram

---
+++ services/AuthenticationService/src/main/java/db/DentistSchema.java
@@ -0,0 +1,55 @@
+package main.java.db;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "dentists")
+public class DentistSchema { 
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private String clinic;
+
+    public String getClinic() {
+        return clinic;
+    }
+    public void setClinic(String clinic) {
+        this.clinic = clinic;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }    
+    public void setEmail(String newEmail){
+        this.email = newEmail;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+    public void setPassword(String newPassword){
+        this.password = newPassword;
+    }
+
+    public String toString(){
+        String dentistRepresentation = "";
+        dentistRepresentation = "{"+ "id: "+ this.id + ", \n" + "name: "+ this.name+ ", \n" + "email: "+ this.email+ ", \n" + "clinic: "+ this.clinic +"}";
+        return dentistRepresentation;
+
+    }
+
+}
