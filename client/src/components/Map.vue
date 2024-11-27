e7e8cd209cd7c3a3eb830ec44535fe55709cbd00
Danis Music <danism@student.chalmers.se>
2024-11-27 17:44:23 +0100

    #10 Add methods for handling markers and views

--- client/src/components/Map.vue
+++ client/src/components/Map.vue
@@ -3,6 +3,7 @@
 </template>
 
 <script>
+import dentistClinicIcon from '../assets/dentistClinicIcon.png'
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
 
@@ -27,10 +28,34 @@ export default {
       } else {
         this.markerGroup = L.layerGroup().addTo(this.map)
       }
-      console.log('Inside addmarker, check clinics: ' + this.clinics)
+
       this.clinics.forEach(clinic => {
-        const marker = L.marker([clinic.coordinate.latitude, clinic.coordinate.longitude]).addTo(this.map)
-        marker.bindPopup(clinic.name)
+        const marker = L.marker([clinic.coordinate.latitude, clinic.coordinate.longitude],
+          {
+            icon: L.icon({
+              iconUrl: dentistClinicIcon,
+              iconSize: [28, 28],
+              iconAnchor: [14, 10]
+            })
+          })
+
+        marker.bindTooltip(clinic.name, {
+          permanent: true,
+          direction: 'top'
+        })
+
+        const popUpContent =
+        `
+        <div>
+          <h3>${clinic.name}</h3>
+          <p><strong>Address: </strong>${clinic.address}</p>
+          <p><strong>Contact Info:</strong> ${clinic.contactInfo.number}, ${clinic.contactInfo.email}</p>
+          <p><strong>Open Hours:</strong> ${clinic.openHours}</p>
+          <p><strong>Dentists: </strong> ${clinic.dentists}</p>
+        </div>
+        `
+        const popUp = L.popup().setContent(popUpContent)
+        marker.bindPopup(popUp)
         this.markerGroup.addLayer(marker)
       })
     }
@@ -44,11 +69,28 @@ export default {
     }
   },
   mounted() {
-    this.map = L.map(this.$refs.map, L.CRS.Simple).setView([57.708870, 11.974560], 7)
+    this.map = L.map(this.$refs.map).setView([57.708870, 11.974560], 7)
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    const streetView = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
       noWrap: true
+    })
+
+    const satelliteView = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+      maxZoom: 20,
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    })
+
+    streetView.addTo(this.map)
+
+    L.control.layers({
+      Street: streetView,
+      Satellite: satelliteView
     }).addTo(this.map)
+
+    if (this.clinics && this.clinics.length > 0) {
+      this.addMarkers()
+    }
   }
 }
 
@@ -61,4 +103,10 @@ export default {
         overflow: hidden;
         border-style: solid;
     }
+
+    .leaflet-tooltip {
+      font-weight: bold;
+      background-size: small;
+    }
+
 </style>
