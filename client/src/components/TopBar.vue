a8760e3481290efa7bf11abdb29d39ea65b3ccfe
Nadman Faisal <nadman@chalmers.se>
2024-12-06 17:55:02 +0100

    #16 Reintroduce selection screen

--- client/src/components/TopBar.vue
+++ client/src/components/TopBar.vue
@@ -31,6 +31,7 @@ export default {
   data() {
     return {
       navItems: [
+        { name: 'Select Clinic', route: '/patientSelection' },
         { name: 'My Bookings', route: '/patientHomePage' }
         // Add more navigation items here
       ]
