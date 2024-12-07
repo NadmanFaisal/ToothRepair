55c3c6824b9645b821bac28e09cfee297a6f1f6f
Danis Music <danism@student.chalmers.se>
2024-12-07 13:15:38 +0100

    #15 Remove duplicate publishing methods

--- client/src/mqtt/mqtt.js
+++ client/src/mqtt/mqtt.js
@@ -77,54 +77,15 @@ export function messageArrived(callback) {
 }
 
 /**
- * This function publishes an alert to the microservice so that the microservice can publish its values.
- *
- * @param {*} topic requires a topic to publish a value
- */
-export function publishToTopic(topic, message) {
-  if (client.connected && topic === 'ScheduleService/Appointment/createAppointment') {
-    console.log('Im trying to publish to the broker')
-    client.publish(topic, message) // publishes Get Appointments as a message to recieve all patients
-    console.log(`I have published to ${message} to ${topic}`)
-  } else if (client.connected && topic === 'ScheduleService/Appointment/getAppointments') {
-    console.log('Im trying to publish to the broker')
-    client.publish(topic, message) // publishes Get Appointments as a message to recieve all patients
-    console.log(`I have published to ${message} to ${topic}`)
-  } else if (client.connected && topic === 'ScheduleService/Appointment/bookAppointment') {
-    console.log('Im trying to publish to the broker')
-    client.publish(topic, message) // publishes Get Appointments as a message to recieve all patients
-    console.log(`I have published to ${message} to ${topic}`)
-  } else if (client.connected && topic === 'ScheduleService/Appointment/changeAppointmentStatus') {
-    console.log('Im trying to publish to the broker')
-    client.publish(topic, message) // publishes Get Appointments as a message to recieve all patients
-    console.log(`I have published to ${message} to ${topic}`)
-  } else if (client.connected && topic === 'test/clinicAlert') {
-    console.log('Im trying to publish to the "test/clinicAlert" topic')
-    client.publish(topic, 'Get Clinics') // publishes Get Patients as a message to recieve all patients
-    console.log('I have published "Get Clinics" to the "test/clinicAlert" topic')
-  } else if (client.connected && topic === 'test/patientAlert') {
-    console.log('Im trying to publish to the broker')
-    client.publish(topic, 'Get Patients') // publishes Get Patients as a message to recieve all patients
-    console.log('I have published get patients to the broker')
-  }
-}
-
-/**
  * This function sends a message to the topic which is provided
  *
  * @param {*} topic specifies the topic that the message is being sent to
  * @param {*} message the message that is sent through the topic
  */
-export function publishMsgToTopic(topic, message) {
+export function publishToTopic(topic, message) {
   if (client.connected) {
     client.publish(topic, message, { qos: 2, retain: false })
-    console.log('Published the message')
+    console.log('Published the message: ' + message + 'to topic: ' + topic)
   }
 }
-export function publishValue(topic, payload) {
-  if (client.connected) {
-    console.log('Publishing ' + payload + ' to ' + topic)
-    client.publish(topic, payload)
-    console.log(payload + ' Has been published to ' + topic)
-  }
-}
+
