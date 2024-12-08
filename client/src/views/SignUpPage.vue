28070caf6da393ba780cf354d7d9237b0533d524
Nadman Faisal <nadman@chalmers.se>
2024-12-08 13:19:30 +0100

    #17 Allow moving to login page

--- client/src/views/SignUpPage.vue
+++ client/src/views/SignUpPage.vue
@@ -1,57 +1,43 @@
 <template>
-    <b-col class="white-background">
-      <b-container fluid class="vh-100 d-flex align-items-center justify-content-center">
-        <b-row class="justify-content-center align-items-center w-100">
-          <b-col md="8" class="signup-blue-container d-flex justify-content-center align-items-center">
+  <div class="col-12 screen-container">
 
-            <!-- Navigable Login/SignUp page-->
-            <b-col class="login-signup-button-container">
-                <BButton type="button" class="login-button" @click="setDentistFalse()">I am a Patient</BButton>
-                <BButton type="button" class="login-button" @click="setDentistTrue()">I am a Dentist</BButton>
-              <BButton type="button" class="login-button" @click="goToLoginPage()">Log In</BButton>
-              <BButton type="button" class="signup-button" @click="goToSignUpPage()">Sign up</BButton>
-            </b-col>
+    <div class="col-7 left-section">
 
-            <b-col class="white-container-signup p-4">
-              <h1 class="signup-title mb-4">Sign Up</h1>
+      <div class="col-12 logo-section">
+        <img src="../assets/app-logo.png" class="logo-image">
+        <h1 class="logo-title">TEETH REPAIR</h1>
+      </div>
 
-              <!-- the whole SignUpForm -->
-              <SignUpForm
-                :username="username"
-                :email="email"
-                :password="password"
-                :clinics="clinics"
-                :isDentist="isDentist"
-                @submit="submitSignUp"
-              />
+      <div class="col-12 login-content-section">
 
-              <!-- checks for error message response -->
-              <p
-                v-if="message"
-                :class="{
-                  'text-success': message === 'Sign Up Successful!',
-                  'text-danger': message !== 'Sign Up Successful!'
-                }"
-                class="mt-3"
-              >
-                {{ message }}
-              </p>
-            </b-col>
-          </b-col>
-        </b-row>
-      </b-container>
-    </b-col>
-  </template>
+        <div class="col-2 left-empty-section">
+        </div>
+
+        <div class="col-8 middle-login-section">
+          <div class="col-12 register-section">
+              <h1 class="register-title">Welcome</h1>
+              <label class="login-label">Already have an account? <span class="highlighted-text" @click="navigateToLoginPage">Log in!</span></label>
+          </div>
+        </div>
+
+        <div class="col-2 right-empty-section">
+        </div>
+
+      </div>
+
+    </div>
+
+    <div class="col-5 right-section">
+    </div>
+
+  </div>
+</template>
 
 <script>
-import SignUpForm from '@/components/SignUpForm.vue'
 import { subscribeToTopic, publishValue, messageArrived, unsubscribeFromTopic } from '../mqtt/mqtt.js'
 
 export default {
   name: 'SignUpPage',
-  components: {
-    SignUpForm
-  },
 
   data() {
     return {
@@ -65,6 +51,9 @@ export default {
   },
 
   methods: {
+    navigateToLoginPage() {
+      this.$router.push('/login')
+    },
     // post the email, name and password of the businessOwner and creates a new one in backend
     async submitSignUp({ username, email, password, clinic }) {
       const PUBLISH_PATIENT_TOPIC = 'patient/authentication/signup'
@@ -158,108 +147,94 @@ export default {
 }
 </script>
 
-  <style scoped>
-  .white-background {
-    background: #FFF;
-    min-height: 100vh;
-  }
+<style scoped>
+.screen-container {
+  display: flex;
+  flex-direction: row;
+  height: 100vh;
+}
 
-  .signup-blue-container {
-    background: #37F;
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    padding: 40px;
-    width: 100%;
-    max-width: 800px;
-    position: relative;
-    border-radius: 0;
-  }
+.left-section {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
 
-  .white-container-signup {
-    width: 100%;
-    max-width: 404px;
-    border-radius: 50px;
-    background: #FFF;
-    box-shadow: -15px 15px 4px 0px rgba(0, 0, 0, 0.25);
-    padding: 40px 60px;
-  }
+.right-section {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  background-image: linear-gradient(0deg, rgba(31, 194, 194, 0.24) 0%, rgba(31, 194, 194, 0.24) 100%), url('@/assets/login-image.jpeg');
+  background-size: contain;
 
-  .signup-title {
-    font-family: 'Istok Web', sans-serif;
-    font-size: 36px;
-    font-weight: 700;
-    color: #000;
-    text-align: center;
-  }
+}
 
-  @media (max-width: 768px) {
-    .signup-blue-container {
-      max-width: 100%;
-      padding: 30px;
-    }
+.logo-section {
+  display: flex;
+  flex-direction: row;
+  height: 15%;
+  align-items: center;
+}
 
-    .white-container-signup {
-      padding: 30px 40px;
-    }
+.logo-image {
+  margin-left: 40px;
+  height: 55%;
+}
 
-    .signup-title {
-      font-size: 28px;
-    }
-  }
+.logo-title {
+  margin-left: 20px;
+  color: #515151;
+  text-align: center;
+  font-family: Inter;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 900;
+  line-height: normal;
+}
 
-  @media (max-width: 576px) {
-    .signup-title {
-      font-size: 24px;
-    }
+.login-content-section {
+  display: flex;
+  flex-direction: row;
+  height: 85%;
+  flex-wrap: wrap;
+}
 
-    .white-container-signup {
-      padding: 20px 30px;
-    }
-  }
+.register-section {
+  display: flex;
+  flex-direction: column;
+  height: 30%;
+  align-items: start;
+  justify-content: center;
 
-  .login-signup-button-container {
-    position: absolute;
-    top: 50px;
-    right: 50px;
-    display: flex;
-    gap: 20px;
-  }
+}
 
-  button.login-button {
-    width: 130px;
-    height: 50px;
-    border: none;
-    border-radius: 50px;
-    background: #FFF;
-    color: #37F;
-    font-family: "Istok Web";
-    font-size: 16px;
-    font-weight: 400;
-    box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
-  }
+.register-title {
+  color: #515151;
+  text-align: center;
+  font-family: Inter;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+}
 
-  button.signup-button {
-    width: 130px;
-    height: 50px;
-    border: none;
-    border-radius: 50px;
-    background: #37F;
-    color: #FFF;
-    font-family: "Istok Web";
-    font-size: 16px;
-    font-weight: 400;
-    line-height: normal;
-    box-shadow: inset 0px 4px 4px rgba(0, 0, 0, 0.25);
-  }
+.login-label {
+  color: #515151;
+  text-align: center;
+  font-family: Inter;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+}
 
-  button.login-button:hover {
-    background-color: rgb(235, 235, 235);
-  }
-
-  button.signup-button:hover {
-    background-color: rgb(0, 85, 255);
-  }
-
+.highlighted-text {
+  color: #1FC2C2;
+  font-family: Inter;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  text-decoration: underline;
+}
 </style>
