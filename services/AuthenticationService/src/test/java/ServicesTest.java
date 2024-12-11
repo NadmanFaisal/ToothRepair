b6e8267024e5a30a9fcd1e4ea9df7c805715234a
Mohamed Taha Jasser <mohamedt@student.chalmers.se>
2024-12-11 18:08:50 +0100

    #18 Add JUnit Patient Duplicate Test
    Co-authored by: @puram

--- services/AuthenticationService/src/test/java/ServicesTest.java
+++ services/AuthenticationService/src/test/java/ServicesTest.java
@@ -1,28 +1,28 @@
 package test.java;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import main.java.db.DentistSchema;
 import main.java.db.PatientSchema;
 import main.java.service.DentistService;
 import main.java.service.PatientService;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class ServicesTest {
     @Mock
     private PatientService patientService;
     @Mock
     private DentistService dentistService;
-    @InjectMocks
     private PatientSchema testPatient;
-    @InjectMocks
     private DentistSchema testDentist;
     
     @BeforeEach
@@ -35,15 +35,17 @@ public class ServicesTest {
 
     @Test
     @DisplayName("Try to search for a patient by email and return true if they exists, false if they dont")
-    void checkDuplicatePatient(){
-        testPatient.setEmail("Vaibhavpuram05@gmail.com");
+    void checkDuplicatePatientTest(){
+        testPatient.setEmail("tokyo@gmail.com");
+        when(patientService.checkDuplicatePatient(any())).thenReturn(true);
         assertEquals(true, patientService.checkDuplicatePatient(testPatient), "Duplicate email should exists and true should be returned");
     }
 
     @Test
     @DisplayName("Try to search for a dentist by email and return true if they exists, false if they dont")
-    void checkDuplicateDentist(){
-        testDentist.setEmail("Vaibhavpuram05@gmail.com");
+    void checkDuplicateDentistTest(){
+        testDentist.setEmail("vaibhavpuram05@gmail.com");
+        when(dentistService.checkDuplicateDentist(any())).thenReturn(true);
         assertEquals(true, dentistService.checkDuplicateDentist(testDentist), "Duplicate email should exists and true should be returned");
     }
 
