053e6be4402ca36b69e32b0f31f78d729f56b261
Nadman Faisal <nadman@chalmers.se>
2024-12-17 22:32:12 +0100

    #20 Make dentist mybookingspage responsive

--- client/src/views/DentistMyBookingsPage.vue
+++ client/src/views/DentistMyBookingsPage.vue
@@ -41,7 +41,7 @@
                       </div>
 
                       <div class="col-4 clinic-details-container">
-                        <label class="clinic-name-label">{{ appointment.clinic }}</label>
+                        <!-- <label class="clinic-name-label">{{ appointment.clinic }}</label> -->
                       </div>
 
                       <div class="col-3 status-container">
@@ -478,4 +478,146 @@ export default {
   font-weight: 600;
   line-height: normal;
   }
-  </style>
+
+  @media (max-width: 1450px) {
+  .book-appointment-button {
+    font-size: 15px;
+  }
+
+  .appointment-image {
+    height: 40px;
+    width: 40px;
+  }
+
+  .appointment-day-label {
+    font-size: 55px;
+  }
+
+  .appointment-month-day-label, .appointment-start-time-span {
+    font-size: 16px;
+  }
+}
+
+@media (max-width: 1220px) {
+  .appointment-image {
+    height: 30px;
+    width: 30px;
+  }
+
+  .appointment-day-label {
+    font-size: 45px;
+  }
+
+  .completed-label {
+    font-size: 22px;
+  }
+
+}
+
+@media (max-width: 1154px) {
+  .book-appointment-button {
+    font-size: 12px;
+  }
+}
+
+@media (max-width: 1100px) {
+  .title-label {
+    font-size: 40px;
+  }
+
+  .book-appointment-label {
+    font-size: 25px;
+  }
+
+  .completed-label {
+    font-size: 18px;
+  }
+}
+
+@media (max-width: 850px) {
+  .content-section {
+    height: 200vh;
+  }
+
+  .main-content-section {
+    flex-direction: column;
+    padding: 15px;
+  }
+
+  .title-container {
+    height: 10%;
+  }
+
+  .appointment-container {
+    height: 40%;
+    width: 98%;
+  }
+
+  .completed-label {
+    font-size: 20px;
+  }
+
+  .maps-container {
+    height: 40%;
+    width: 98%;
+  }
+}
+
+@media (max-width: 680px) {
+  .book-appointment-button {
+    width: 70%;
+    font-size: 8px;
+  }
+
+  .appointment-image {
+    height: 25px;
+    width: 25px;
+  }
+
+  .appointment-day-label {
+    font-size: 30;
+  }
+
+  .appointment-month-day-label, .appointment-start-time-span {
+    font-size: 12px;
+  }
+
+  .completed-label {
+    font-size: 15px;
+  }
+}
+
+@media (max-width: 560px) {
+  .logo-container {
+    display: none;
+  }
+}
+
+@media (max-width: 510px) {
+  .title-label {
+    font-size: 30px;
+  }
+
+  .book-appointment-label {
+    font-size: 20px;
+  }
+
+  .cancel-button {
+    font-size: 15px;
+  }
+}
+
+@media (max-width: 380px) {
+  .title-label {
+    font-size: 25px;
+  }
+
+  .completed-label {
+    font-size: 12px;
+  }
+
+  .book-appointment-label {
+    font-size: 15px;
+  }
+}
+</style>
