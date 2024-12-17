701500a8f62da822b602b01fc2a92521ec37a224
Mohamed Taha Jasser <mohamedt@student.chalmers.se>
2024-12-17 18:31:37 +0100

    #19 Remove unnecessary comments

--- mqtt_test.sh
+++ mqtt_test.sh
@@ -1,31 +1,8 @@
-#!/bin/bash
-#apt-get update
-#cd ~
-
-# Install curl
-#apt-get install curl -y
-
-# Install Go using curl
-#curl https://raw.githubusercontent.com/canha/golang-tools-install-script/master/goinstall.sh | bash
-#export PATH=$PATH:/root/bin
-#go version
-
-# Install mqtt-benchmark with Go
-#go install github.com/krylovsk/mqtt-benchmark@main
-
-#     might be useful for CI pipeline
-#apt-get update && apt-get install -y curl git
-#curl -LO https://go.dev/dl/go1.20.0.linux-amd64.tar.gz
-#tar -C /usr/local -xzf go1.20.0.linux-amd64.tar.gz
-#export PATH=$PATH:/usr/local/go/bin
-#go version
-#go install github.com/krylovsk/mqtt-benchmark@main
-
 # Base configuration
 base_email="toothrepair356@gmail.com"
 broker="wss://193a0f31e34647d9a74f1e130a9238ba.s1.eu.hivemq.cloud:8884/mqtt"
 password="1234567890"
-clients=10  # Set the amount of clients you wan to run concurrently
+clients=10  # Set the amount of clients you wan to run concurrently (left at 10 because this is very demanding to the local broker, only run 1000s for manual testing)
 
 # Stress-test authentication service (to signup clients)
 for i in $(seq 1 $clients); do
