0ecc5b4a30a8192ca9b895eba83cea1cd488024c
Mohamed Taha Jasser <mohamedt@student.chalmers.se>
2024-12-23 21:22:03 +0300

    #21 Add Stress test get appointments by patient id

--- mqtt_test.sh
+++ mqtt_test.sh
@@ -1,6 +1,9 @@
 # Base configuration
 base_email="toothrepair356@gmail.com"
 broker="wss://193a0f31e34647d9a74f1e130a9238ba.s1.eu.hivemq.cloud:8884/mqtt"
+clinic_id="674a01ce4d3aa16b1e5f5f4a"
+patient_id="67683551693b7b734d000cb3"
+dentist_id="67683482693b7b734d000cb1"
 password="1234567890"
 clients=10  # Set the amount of clients you wan to run concurrently (left at 10 because this is very demanding to the local broker, only run 1000s for manual testing)
 
@@ -47,6 +50,13 @@ clients=10  # Set the amount of clients you wan to run concurrently (left at 10 
 #done
 
 # Stress-test schedule service (get available appointments)
+# Stress-test schedule service (get appointments by patient)
+#for i in $(seq 1 $clients); do
+#  payload="{\"patient\": \"$patient_id\"}"
+#  mqtt-benchmark --broker "$broker" --count 1 --clients 1 --insecure --username "Administrator" --password "Vaibhav12Taha" --qos 2 --topic "ScheduleService/Appointment/getAppointmentsByPatient" --client-prefix "mqtt-client-$i" --payload "$payload" &
+#done
+
+#wait
 for i in $(seq 1 $clients); do
   payload="{\"Get Available Appointments $i\"}"
   #payload="Get Available Appointments"
