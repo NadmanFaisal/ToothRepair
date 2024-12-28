d0933e0686ac1ea8945491c1046d8b2d47dabce3
Nadman Faisal <nadman@chalmers.se>
2024-12-28 18:08:45 +0100

    #22 Automate the building of docker microservices with replicas

--- automated_build.sh
+++ automated_build.sh
@@ -1,14 +1,30 @@
-# Add other services if created in the future
+#!/bin/bash
+
+# Project service directory names
 SERVICES=("AuthenticationService" "ClinicService" "NotificationService" "ScheduleService")
 
-# Script to build each services
-for SERVICE in "${SERVICES[@]}"
+# Docker image services names in GitLab Container Registry
+IMAGES=("authentication-service" "clinic-service" "notification-service" "schedule-service")
+
+REGISTRY="registry.git.chalmers.se/courses/dit355/2024/student_teams/dit356_2024_20/dit-356-project-group-20"
+
+for i in "${!SERVICES[@]}"
 do
-  echo "Building $SERVICE..."
+  SERVICE="${SERVICES[$i]}"
+  IMAGE="${IMAGES[$i]}"
+
+  echo "Building $SERVICE -> $IMAGE..."
+
   cd "services/$SERVICE" || exit
+
   mvn clean package || { echo "Build failed for $SERVICE"; exit 1; }
+
+  docker build -t "$REGISTRY/$IMAGE" .
+
+  docker push "$REGISTRY/$IMAGE"
+
   cd - || exit
 done
 
-# Build the docker compose file
-docker-compose up --build
+# Deploy the services as stack to Docker Swarm
+docker stack deploy -c docker-compose.yml microservices_stack
